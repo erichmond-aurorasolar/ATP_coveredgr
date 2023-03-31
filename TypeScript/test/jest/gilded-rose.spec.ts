18691958c38e81af713a963f6e34fa3e00706932
Ivan Gonzalez <6606619+gonzalezi2@users.noreply.github.com>
2023-03-31 11:04:51 -0700

    add conjured item, update tests

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,10 +1,11 @@
-import { Item, GildedRose } from '@/gilded-rose';
-
+import { Item, GildedRose } from "@/gilded-rose";
 
 describe("Gilded Rose", function () {
   it("Legendary Item quality does not decrease", () => {
     // Arrange
-    const sut = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", 20, 80)]);
+    const sut = new GildedRose([
+      new Item("Sulfuras, Hand of Ragnaros", 20, 80),
+    ]);
 
     // Act
     const items = sut.updateQuality();
@@ -22,13 +23,14 @@ describe("Gilded Rose", function () {
   });
 
   it("Legendary Item quality does not change after sellin", () => {
-    const sut = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", -1, 80)]);
+    const sut = new GildedRose([
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    ]);
 
     const items = sut.updateQuality();
 
     expect(items[0].quality).toBe(80);
   });
-
 
   it("Generic item SellIn decreases each update", () => {
     const sut = new GildedRose([new Item("generic item", 8, 8)]);
@@ -38,21 +40,44 @@ describe("Gilded Rose", function () {
     expect(items[0].sellIn).toBe(7);
   });
 
-  // TODO - can the tests for `negative after sellIn date reached` be parameterized for several items?? 
-  it("Generic item SellIn will be negative after sellIn date reached", () => {
-    const sut = new GildedRose([new Item("generic item", 0, 10)]);
+  // TODO - can the tests for `negative after sellIn date reached` be parameterized for several items??
+
+  it.each([
+    ["generic item"],
+    ["Aged Brie"],
+    ["Backstage passes"],
+    ["Conjured"],
+  ])("%p sellIn will be negative after sellIn date reached", (item) => {
+    const sut = new GildedRose([new Item(item, 0, 10)]);
 
     const items = sut.updateQuality();
 
     expect(items[0].sellIn).toBe(-1);
   });
 
-  it("Generic item quality decreases by 1 before sellIn date reached", () => {
-    const sut = new GildedRose([new Item("generic item", 5, 10)]);
+  it.each([
+    ["generic item", 1],
+    ["Conjured", 2],
+  ])(
+    "%p quality decreases by %p before sellIn date reached",
+    (item, decrement) => {
+      const sut = new GildedRose([new Item(item, 5, 10)]);
+
+      const items = sut.updateQuality();
+
+      expect(items[0].quality).toBe(10 - decrement);
+    }
+  );
+
+  it.each([
+    ["generic item", 1],
+    ["Conjured", 2],
+  ])("%p quality never decreases below 0", (item, decrement) => {
+    const sut = new GildedRose([new Item(item, 5, 10)]);
 
     const items = sut.updateQuality();
 
-    expect(items[0].quality).toBe(9);
+    expect(items[0].quality).toBe(10 - decrement);
   });
 
   it("Generic item quality decreases twice as fast after sellIn date reached", () => {
@@ -71,22 +96,6 @@ describe("Gilded Rose", function () {
     expect(items[0].quality).toBe(0);
   });
 
-  it("Aged Brie item SellIn decreases each update", () => {
-    const sut = new GildedRose([new Item("Aged Brie", 5, 5)]);
-
-    const items = sut.updateQuality();
-
-    expect(items[0].sellIn).toBe(4);
-  });
-
-  it("Aged Brie SellIn will be negative after sellIn date reached", () => {
-    const sut = new GildedRose([new Item("Aged Brie", 0, 25)]);
-
-    const items = sut.updateQuality();
-
-    expect(items[0].sellIn).toBe(-1);
-  });
-
   it("Aged Brie quality improves with age", () => {
     const sut = new GildedRose([new Item("Aged Brie", 5, 30)]);
 
@@ -103,7 +112,7 @@ describe("Gilded Rose", function () {
     expect(items[0].quality).toBe(32);
   });
 
-  // TODO: can all items which improve with and have cap be tested together? 
+  // TODO: can all items which improve with and have cap be tested together?
   it("Aged Brie improves with age, quality is capped at 50 before sellIn date reached", () => {
     const sut = new GildedRose([new Item("Aged Brie", 10, 50)]);
 
@@ -120,26 +129,10 @@ describe("Gilded Rose", function () {
     expect(items[0].quality).toBe(50);
   });
 
-
-  // TODO - there are several items whose Sellin decreases each update.. can we make it dat driven? 
-  it("Backstage passes SellIn decreases each update", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]);
-
-    const items = sut.updateQuality();
-
-    expect(items[0].sellIn).toBe(9);
-  });
-
-  it("Backstage passes SellIn will be negative after sellIn date reached", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 25)]);
-
-    const items = sut.updateQuality();
-
-    expect(items[0].sellIn).toBe(-1);
-  });
-
   it("Backstage pass quality increases when concert is far in the future", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 30, 23)]);
+    const sut = new GildedRose([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 30, 23),
+    ]);
 
     const items = sut.updateQuality();
 
@@ -147,7 +140,9 @@ describe("Gilded Rose", function () {
   });
 
   it("Backstage pass quality increases more when concert is 10 days away", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 23)]);
+    const sut = new GildedRose([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 23),
+    ]);
 
     const items = sut.updateQuality();
 
@@ -155,16 +150,19 @@ describe("Gilded Rose", function () {
   });
 
   it("Backstage pass quality increases even more when concert is 5 days away", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 23)]);
+    const sut = new GildedRose([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 23),
+    ]);
 
     const items = sut.updateQuality();
 
     expect(items[0].quality).toBe(26);
   });
 
-
   it("Backstage passes improves with age, quality is capped at 50", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)]);
+    const sut = new GildedRose([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50),
+    ]);
 
     const items = sut.updateQuality();
 
@@ -172,7 +170,9 @@ describe("Gilded Rose", function () {
   });
 
   it("Backstage pass quality drops to zero when concert has passed", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 23)]);
+    const sut = new GildedRose([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 23),
+    ]);
 
     const items = sut.updateQuality();
 
@@ -180,7 +180,9 @@ describe("Gilded Rose", function () {
   });
 
   it("Backstage pass quality is capped at 50 when concert is about to happen", () => {
-    const sut = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49)]);
+    const sut = new GildedRose([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49),
+    ]);
 
     const items = sut.updateQuality();
 
@@ -188,16 +190,48 @@ describe("Gilded Rose", function () {
   });
 
   it("Shop can contain multiple items and each is updated", () => {
-    const sut = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-    new Item("generic item", 10, 5)]);
+    const sut = new GildedRose([
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("generic item", 10, 5),
+    ]);
 
     const items = sut.updateQuality();
 
     // jest doesn't have a mechanism to ensure all expectations are verified
-    // Is there any other way to do this? 
+    // Is there any other way to do this?
     expect(items[0].sellIn).toBe(0);
     expect(items[1].sellIn).toBe(9);
   });
 
-});
+  it("Conjured item quality decreases four times as fast after sellIn date reached", () => {
+    const sut = new GildedRose([new Item("Conjured", 0, 10)]);
 
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(6);
+  });
+
+  it("Conjured item quality never decreases below 0 after sellIn date reached if quality is three", () => {
+    const sut = new GildedRose([new Item("Conjured", 0, 3)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Conjured item quality never decreases below 0", () => {
+    const sut = new GildedRose([new Item("Conjured", 5, 0)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Conjured item quality never decreases below 0 if quality is one", () => {
+    const sut = new GildedRose([new Item("Conjured", 5, 1)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(0);
+  });
+});
